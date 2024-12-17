3ad94daab518e303b130e902983b63c9a23a9b2c
AkiraAKI114514 <2641063263@qq.com>
2024-12-17 18:43:01 +0800

    Update tools.h

--- final_assgin/tools.h
+++ final_assgin/tools.h
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "msg.h"
 
 #define ADMIN_ID "admin"
@@ -10,29 +11,37 @@
 #define MAX_EPLY_NUMBER 50
 #define MAX_NAME_LEN 50
 #define FLOORS 4
+#define DEFAULT_STAT "Empty"
+#define LIVEIN_STAT "Livein"
+#define BOOKED_STAT "Booked"
 
-typedef struct {
+typedef struct
+{
     char department[100];
     char name[MAX_NAME_LEN];
     char ID[MAX_ID_LEN];
     char password[MAX_PWD_LEN];
 } Employee;
 
-typedef struct {
+typedef struct
+{
     int number;
     int bill;
     char name[MAX_NAME_LEN];
     char ID[MAX_ID_LEN];
     char password[MAX_PWD_LEN];
     char room_number[4];
+    char room_status[10];
 } Customer;
 
 Employee eply_Info[MAX_EPLY_NUMBER];
 Customer cus_Info[MAX_ROOMS];
 
+
 int rooms[MAX_ROOMS];
 
-char* index_to_room(int index) {
+char *index_to_room(int index)
+{
     int rooms_per_floor = MAX_ROOMS / FLOORS;
     int floor_number = index / rooms_per_floor;
     char prefix[4] = {'G', '1', '2', '3'};
@@ -44,15 +53,18 @@ char* index_to_room(int index) {
     return room;
 }
 
-int room_to_index(char *room) {
+int room_to_index(char *room)
+{
     int rooms_per_floor = MAX_ROOMS / FLOORS;
     int index;
     int floor;
     int room_number = atoi(&room[1]);
     char prefix[4] = {'G', '1', '2', '3'};
-    
-    for (int i = 0; i < FLOORS; i++) {
-        if (prefix[i] == room[0]) {
+
+    for (int i = 0; i < FLOORS; i++)
+    {
+        if (prefix[i] == room[0])
+        {
             floor = i;
             break;
         }
@@ -62,27 +74,103 @@ int room_to_index(char *room) {
     return index;
 }
 
-int int_input_check(int *input) {   //check whether the input is valid when needs a integer
-    if (scanf("%d", input) != 1) {
+int find_empty_rooms()
+{
+    int tempend = 0;
+    int tempst = 0;
+    int rooms = MAX_ROOMS;
+    int floor = 0;
+    printf("Empty rooms:\n");
+    for (int i = 0; i < MAX_ROOMS; i++)
+    {
+        // printf("%d", i);
+        if (strcmp(cus_Info[i].room_status, DEFAULT_STAT) == 0)
+        {
+            if ((i + 1) % 50 == 0)
+            {
+                if (tempst != tempend)
+                {
+                    char *st = index_to_room(tempst);
+                    printf("%s", st);
+                    char *end = index_to_room(tempend);
+                    printf("-%s\n", end);
+                }
+                else
+                {
+                    char *end = index_to_room(tempend);
+                    printf("%s\n", end);
+                }
+                tempst = i + 1;
+            }
+        }
+        else
+        {
+            if (tempend - 1 == tempst)
+            {
+                printf("%s ", index_to_room(tempst));
+                tempst = i + 1;
+            }
+            else
+            {
+                char *st1 = index_to_room(tempst);
+                printf("%s-", st1);
+                char *end1 = index_to_room(tempend - 1);
+                printf("%s ", end1);
+
+                tempst = i + 1;
+            }
+            if ((i + 1) % 50 == 0)
+            {
+                printf("\n");
+            }
+        }
+        tempend += 1;
+    }
+    return 0;
+}
+
+int init()
+{
+    for (int i = 0; i < MAX_ROOMS; i++)
+    {
+        strcpy(cus_Info[i].room_status, DEFAULT_STAT);
+    }
+}
+
+int int_input_check(int *input)
+{ // check whether the input is valid when needs a integer
+    if (scanf("%d", input) != 1)
+    {
         printf("[ERROR] Invalid input,please try again!\n");
 
-        while (getchar() != '\n');
+        while (getchar() != '\n')
+            ;
         return 0;
-    } else {
+    }
+    else
+    {
         return 1;
     }
 }
 
-int find_ID(char *identity, char *ID) {
-    if (strcmp(identity, "Employee") == 0) {
-        for (int i = 0; i < MAX_EPLY_NUMBER; i++) {
-            if (strcmp(eply_Info[i].ID, ID) == 0) {
+int find_ID(char *identity, char *ID)
+{
+    if (strcmp(identity, "Employee") == 0)
+    {
+        for (int i = 0; i < MAX_EPLY_NUMBER; i++)
+        {
+            if (strcmp(eply_Info[i].ID, ID) == 0)
+            {
                 return i;
             }
         }
-    } else if (strcmp(identity, "Customer") == 0) { 
-        for (int i = 0; i < MAX_ROOMS; i++) {
-            if (strcmp(cus_Info[i].ID, ID) == 0) {
+    }
+    else if (strcmp(identity, "Customer") == 0)
+    {
+        for (int i = 0; i < MAX_ROOMS; i++)
+        {
+            if (strcmp(cus_Info[i].ID, ID) == 0)
+            {
                 return i;
             }
         }
@@ -90,14 +178,17 @@ int find_ID(char *identity, char *ID) {
     return -1;
 }
 
-int login(char *identity, char *ID){
+int login(char *identity, char *ID)
+{
     char pwd[MAX_PWD_LEN];
 
-    while (1) {
+    while (1)
+    {
         printf("[INPUT] Please enter your %s ID >> ", identity);
         scanf(" %[^\n]", ID);
-        
-        if (strcmp(ID, "Exit") == 0) {
+
+        if (strcmp(ID, "Exit") == 0)
+        {
             printf("[INFO] Returning to main menu.\n");
             return 0;
         }
@@ -105,33 +196,47 @@ int login(char *identity, char *ID){
         printf("[INPUT] Please enter your password >> ");
         scanf(" %[^\n]", pwd);
 
-        if (strcmp(pwd, "Exit") == 0) {
+        if (strcmp(pwd, "Exit") == 0)
+        {
             printf("[INFO] Returning to main menu.\n");
             return 0;
         }
 
-        if (strcmp(identity, "Admin") == 0) {
-            if (strcmp(ID, ADMIN_ID) == 0) {
-                if (strcmp(pwd, ADMIN_PWD) == 0) {
+        if (strcmp(identity, "Admin") == 0)
+        {
+            if (strcmp(ID, ADMIN_ID) == 0)
+            {
+                if (strcmp(pwd, ADMIN_PWD) == 0)
+                {
                     printf("[INFO] Welcome, Administrator.\n");
-                    return 1; 
-                } else {
+                    return 1;
+                }
+                else
+                {
                     printf("[ERROR] Wrong password, please try again!\n");
                     return 0;
                 }
             }
-        } else if (strcmp(identity, "Employee") == 0 || strcmp(identity, "Customer") == 0) {
+        }
+        else if (strcmp(identity, "Employee") == 0 || strcmp(identity, "Customer") == 0)
+        {
             int pos = find_ID(identity, ID);
 
-            if (pos != -1) {
-                if (strcmp(identity, "Employee") == 0) {
-                    if (strcmp(pwd, eply_Info[pos].password) == 0) {
-                        printf("[INFO] Welcome, %s: %s.\n",identity,eply_Info[pos].name);
+            if (pos != -1)
+            {
+                if (strcmp(identity, "Employee") == 0)
+                {
+                    if (strcmp(pwd, eply_Info[pos].password) == 0)
+                    {
+                        printf("[INFO] Welcome, %s: %s.\n", identity, eply_Info[pos].name);
                         return 1;
                     }
-                } else {
-                    if (strcmp(pwd, cus_Info[pos].password) == 0) {
-                        printf("[INFO] Welcome, %s: %s.\n",identity,cus_Info[pos].name);
+                }
+                else
+                {
+                    if (strcmp(pwd, cus_Info[pos].password) == 0)
+                    {
+                        printf("[INFO] Welcome, %s: %s.\n", identity, cus_Info[pos].name);
                         return 1;
                     }
                 }
