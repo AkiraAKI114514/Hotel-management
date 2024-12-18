6380bfbcb6f5e371b6afc9449e1201cc3af494c9
AkiraAKI114514 <2641063263@qq.com>
2024-12-19 02:07:00 +0800

    Update msg.h

--- final_assgin/msg.h
+++ final_assgin/msg.h
@@ -16,3 +16,19 @@ void menuHelpMsg() {
     printf("4. Exit\n");
     printf("[INPUT] Please select an option (1-4) >> ");
 }
+
+void adminHelpMsg(){
+    printf("[Administrator Menu]\n");
+    printf("1. Record Room Infomation\n");
+    printf("2. View Customers' infomation\n");
+    printf("3. Exit\n");
+    printf("[INPUT] Please select an option (1-3) >>");
+}
+
+void RoomstatMsg(){
+    printf("[Select the status]\n");
+    printf("1. Empty\n");
+    printf("2. Booked\n");
+    printf("3. Livein\n");
+    printf("[INPUT] Please select an option (1-3) >>");
+}
